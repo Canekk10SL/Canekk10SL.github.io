d285d13cc9b54dcc31f5589417005b4645cf85dc
Canekk10SL <93083793+Canekk10SL@users.noreply.github.com>
2022-06-12 13:45:08 +0300

    Add files via upload

---
+++ t3/z1.js
@@ -0,0 +1,38 @@
+/*alert(`Привіт`);
+let age = confirm(`Вам є 14 років `);
+
+let x = `вася` ;
+let e = 23;
+
+
+
+
+let age = prompt(`Яка ціна картоплі ?`);
+
+if( age > 10  ){
+	alert(`дорого  `);	
+}else{
+	alert(`дешево`);
+}
+
+
+alert (`Вітаю на сайті MovieList`);
+let age = prompt (`Як вас звати ?`);
+alert (`Привіт ${age}`);
+
+let ega = confirm(`Вам вже є 18 років ?`);
+
+if( ega== true  ){
+
+	alert(`Вартіть квитка становить 100 грн`);	
+}else{
+alert(`Акційна вартість квитка на фільм становить 70 грн`);}*/
+
+
+let age = prompt(`введіть свій email `);
+
+if( age = korin ){
+	alert(`правильно   `);	
+}else{
+	alert(` користувача з такою почтою не існує`);
+}
