f543d39d7477dfd44035cc23d55acabde2a3c6b2
Canekk10SL <93083793+Canekk10SL@users.noreply.github.com>
2022-06-19 13:51:06 +0300

    Add files via upload

---
+++ t4/z1.js
@@ -0,0 +1,42 @@
+
+ /*
+ let name = prompt(`Яке ваше ім'я ?`);
+ if(name == `Петро` || name == `петро`){
+	 alert(`Ти молодець`);	 
+ }else if (name == `Олексій`){
+	 alert(`апччхи`);
+ }else{
+	 alert(`Привіт`);
+ 
+ let name = prompt(`ведіть колір`);
+ if (name == `Зелений` || name == `Green`){
+	 alert(`можна рухатись`);		 
+	 }else if(name == `жовтий` || name == `yellow` ){
+		 alert(`приготуйтесь`);
+	 }else if(name == `червоний ` || name == `red` ){
+		alert(`Стоп`); 
+	 }}*/
+	 let name = prompt (`в якому році виграв золотий мяч Андрій Шевченко`);
+	 let x = prompt (`як звати вашого президента `);
+	 let y = (confirm (`ви людина `) == true);
+	 let h = (confirm (`ви курите `) == false;
+	 let x = 0 
+	 
+	 if ( name == `2004`)
+	 
+	 {
+		 x++;
+		 
+	 }
+	 if (x == `Зеленський`){
+		x++;
+		
+	 }
+	 if (y == true );{
+		 x++;
+		 
+	 }
+	 if (h == false);{
+		 x++;
+	 }
+	 
